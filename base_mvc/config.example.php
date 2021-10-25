774e1136dd8c48055812b1465243a15c54689079
Dieter De Weirdt <dieter.de.weirdt@trendsco.com>
2021-10-25 11:20:11 +0200

    add config example

---
+++ base_mvc/config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+//rename config.example.php to config.php
+
+CONST BASE_DIR = __DIR__;
+
+CONST BASE_URL = '';
+
+CONST DB_DSN = 'mysql:dbname=cms;host=127.0.0.1;port=3306';
+CONST DB_USER = '';
+CONST DB_PWD = '';
